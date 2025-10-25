ae68962278cd7def7c65267badb8071f73c2dd88
PimiTree <voronenkotg@gmail.com>
2025-10-25 19:58:30 +0300

    build(lint): add linter/formater

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import type React from "react"
+import type React from 'react';
 
 const App: React.FC = () => {
   return (
     <h1>MEMORA CARDS</h1>
-  )
-}
+  );
+};
 
-export default App
+export default App;
