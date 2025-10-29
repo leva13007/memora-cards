40339117aa01a1b10fcb97e050cc634eb6766adc
Oleh Levchenko <leva13007@gmail.com>
2025-10-29 18:52:47 +0000

    feat(MC-15) Added Storybook checks and display

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,6 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+// import storybook from 'eslint-plugin-storybook';
+
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from 'typescript-eslint';
