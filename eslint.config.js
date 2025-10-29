1d4c379eb095741fd15a0de3046bc12458ce2b86
PimiTree <voronenkotg@gmail.com>
2025-10-29 17:55:23 +0200

    build(lint): add linter/formater (#37)

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,35 @@
-import js from '@eslint/js'
-import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
+import stylistic from '@stylistic/eslint-plugin'
 import { defineConfig, globalIgnores } from 'eslint/config'
+import rules from './eslint.rules.js'
+import globals from 'globals'
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(['dist', 'node_modules']),
   {
-    files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
-    ],
+    files: ['**/*.{ts,tsx,js,mts,cts}'],
+    plugins: {
+      '@typescript-eslint': tseslint.configs.recommended[0].plugins['@typescript-eslint'],
+      'react-hooks': reactHooks.configs['recommended-latest'].plugins['react-hooks'],
+      '@stylistic': stylistic.configs['recommended'].plugins['@stylistic'],
+      'react-refresh': reactRefresh.configs.vite.plugins['react-refresh']
+    },
+    rules: {
+      ...rules.ts,
+      ...rules.js,
+      ...rules.reactHooks,
+      ...rules.stylistic,
+      ...reactRefresh.configs.vite.rules
+    },
     languageOptions: {
       ecmaVersion: 2022,
-      globals: globals.browser,
-    },
-  },
+      sourceType: 'module',
+      parser: tseslint.parser,
+      globals: {
+        ...globals.browser
+      }
+    }
+  }
 ])
