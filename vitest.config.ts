6b6d9252cf4285ae1ce08882a48e5a0dfde51ebb
Oleh Levchenko <leva13007@gmail.com>
2025-11-02 22:10:42 +0000

    feat(MC-10): Setup Husky

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,22 +1,65 @@
-import { defineConfig } from 'vitest/config';
-import react from '@vitejs/plugin-react';
+import { defineConfig, defineProject, mergeConfig } from 'vitest/config';
+import { playwright } from '@vitest/browser-playwright';
 
-export default defineConfig({
-  plugins: [react()],
-  test: {
-    environment: 'jsdom',
-    setupFiles: ['./test-setup.ts'],
-    globals: true,
-    coverage: {
-      provider: 'v8',
-      reporter: ['text', 'json', 'html'],
-      exclude: [
-        'node_modules/',
-        'dist/',
-        '**/*.d.ts',
-        'vite.config.ts',
-        'vitest.config.ts'
+import { storybookTest } from '@storybook/addon-vitest/vitest-plugin';
+
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const dirname = path.dirname(fileURLToPath(import.meta.url));
+
+import viteConfig from './vite.config';
+
+export default mergeConfig(
+  viteConfig,
+  defineConfig({
+    test: {
+      coverage: {
+        provider: 'v8',
+        reporter: ['text', 'json', 'html'],
+        exclude: [
+          'node_modules/',
+          'dist/',
+          '**/*.d.ts',
+          'vite.config.ts',
+          'vitest.config.ts',
+          'src/**/*.stories.tsx',
+          '.storybook/**'
+        ]
+      },
+      projects: [
+        defineProject({
+          test: {
+            name: 'react',
+            globals: true,
+            environment: 'jsdom',
+            setupFiles: ['./test-setup.ts']
+          }
+        }),
+        defineProject({
+          plugins: [
+            storybookTest({
+              // The location of your Storybook config, main.js|ts
+              configDir: path.join(dirname, '.storybook'),
+              // This should match your package.json script to run Storybook
+              // The --no-open flag will skip the automatic opening of a browser
+              storybookScript: 'yarn storybook --no-open'
+            })
+          ],
+          test: {
+            name: 'storybook',
+            // Enable browser mode
+            browser: {
+              enabled: true,
+              // Make sure to install Playwright
+              provider: playwright({}),
+              headless: true,
+              instances: [{ browser: 'chromium' }]
+            },
+            setupFiles: ['./.storybook/vitest.setup.ts']
+          }
+        })
       ]
     }
-  }
-});
+  })
+);
