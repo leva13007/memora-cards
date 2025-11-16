af724d6adee8277f6a68330f90655800fd6553c4
PimiTree <voronenkotg@gmail.com>
2025-11-16 19:57:46 +0200

    feat: add Text components

---
+++ src/shared/ui/atoms/Text/Text.tsx
@@ -0,0 +1,70 @@
+import React from 'react';
+import styles from './Text.module.css';
+import { cx } from '../../../../utils';
+
+export interface TextProps {
+  children: React.ReactNode
+  className?: string[]
+  style?: React.CSSProperties
+  as?: 'span' | 'p'
+  variant?: 'primary' | 'warning'
+  ['data-testid']?: string
+}
+
+type CssVariantKey = 'text-medium' | 'text-light' | 'text-bold';
+
+const TextBase: React.FC<TextProps & { cssVariantKey: CssVariantKey }> = ({ children, className, style, variant, as, 'data-testid': dataTestId = 'text-base', cssVariantKey }) => {
+  const As = as || 'span';
+
+  const classes = cx(
+    'text',
+    styles[cssVariantKey],
+    variant === 'warning' ? 'warning' : 'primary',
+    ...(className || [])
+  );
+
+  return (
+    <As style={style} className={classes} data-testid={dataTestId}>{children}</As>
+  );
+};
+
+export const TextMedium: React.FC<TextProps> = ({ children, className, style, variant, as, 'data-testid': dataTestId = 'text-medium' }) => {
+  return (
+    <TextBase
+      children={children}
+      className={className}
+      style={style}
+      variant={variant}
+      as={as}
+      data-testid={dataTestId}
+      cssVariantKey="text-medium"
+    />
+  );
+};
+
+export const TextLight: React.FC<TextProps> = ({ children, className, style, variant, as, 'data-testid': dataTestId = 'text-light' }) => {
+  return (
+    <TextBase
+      children={children}
+      className={className}
+      style={style}
+      variant={variant}
+      as={as}
+      data-testid={dataTestId}
+      cssVariantKey="text-light"
+    />
+  );
+};
+export const TextBold: React.FC<TextProps> = ({ children, className, style, variant, as, 'data-testid': dataTestId = 'text-bold' }) => {
+  return (
+    <TextBase
+      children={children}
+      className={className}
+      style={style}
+      variant={variant}
+      as={as}
+      data-testid={dataTestId}
+      cssVariantKey="text-bold"
+    />
+  );
+};
